b7384114255db2783841b8aa0f7b698aeb3f1bd2
Delmondes <wellington.apk@gmail.com>
2024-09-16 20:54:30 -0300

    Hello word em C.

---
+++ helloword/hello-word.c
@@ -0,0 +1,10 @@
+//
+// Created by delmondes on 16/09/24.
+//
+
+#include "hello-word.h"
+#include <stdio.h>
+
+int main() {
+  printf("hello world\n");
+}
---
+++ helloword/hello-word.h
@@ -0,0 +1,8 @@
+//
+// Created by delmondes on 16/09/24.
+//
+
+#ifndef HELLO_WORD_H
+#define HELLO_WORD_H
+
+#endif //HELLO_WORD_H

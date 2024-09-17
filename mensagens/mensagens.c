99a142b621093d0af2e3f30e1d2728eace7ea048
Delmondes <wellington.apk@gmail.com>
2024-09-17 16:56:05 -0300

    mensagens

---
+++ mensagens/mensagens.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Valor inteiro: %d. \n", 10);
+    printf("Valor real: %f. \n", 3.141592);
+    printf("Valor real 2 casas: %.2f. \n", 3.141592);
+    printf("Dado de texto: %c. \n", 'a');
+    printf("Valor inteiro: %s. \n", "testando");
+}
